ec6bdd68af28a9b860f96a14b2fa32fb38e39911
Samuel Pereira <samu.pereira17@hotmail.com>
2023-01-13 17:32:46 -0300

    Helpers Plugin

--- CoffeeApp/CoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
+++ CoffeeApp/CoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
@@ -1,19 +1,28 @@
-﻿using System;
+﻿using MvvmHelpers;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace CoffeeApp.ViewModels
 {
-    public class CoffeeEquipmentViewModel : BindableObject
+    public class CoffeeEquipmentViewModel : ViewModelBase
     {
         public CoffeeEquipmentViewModel()
         {
             IncreaseCount = new Command(OnIncrease);
             DecreaseCount = new Command(OnDecrease);
+            CallServerCommand = new Command(async () => { await CallServer(); });
+            Coffee = new ObservableRangeCollection<string>();
+            Title = "Coffee Equipment";
         }
 
+        public ObservableRangeCollection<string> Coffee { get; }
+
+        public ICommand CallServerCommand { get; }
         public ICommand IncreaseCount { get; }
         public ICommand DecreaseCount { get; set; }
 
@@ -23,16 +32,12 @@ namespace CoffeeApp.ViewModels
         public string CountDisplay
         {
             get => countDisplay;
-            set
-            {
-                if (value == countDisplay)
-                    return;
+            set => SetProperty(ref countDisplay, value);
+        }
 
-                countDisplay = value;
-
-                //Parametro de property change é pego automaticamente, pois está dentro da propriedade
-                OnPropertyChanged();
-            }
+        async Task CallServer()
+        {
+            Coffee.Add(string.Empty);
         }
 
         void OnIncrease()
---
+++ CoffeeApp/CoffeeApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,12 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeApp.ViewModels
+{
+    public class ViewModelBase : BaseViewModel
+    {
+
+    }
+}
